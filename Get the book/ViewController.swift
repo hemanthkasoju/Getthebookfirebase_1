bd19df3556e24bc7a2a512aebfe44991411a61bc
Administrator <HKX532@udml9.cs.uregina.ca>
2018-12-04 21:33:36 -0600

    Initial Commit

---
+++ Get the book/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Get the book
+//
+//  Created by Hemanth Kasoju on 2018-12-04.
+//  Copyright © 2018 Hemanth Kasoju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
